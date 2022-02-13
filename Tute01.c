71a5fe608f2d00128b6251c1accc03893ad46631
IT21263262 <99611815+it21263262@users.noreply.github.com>
2022-02-13 18:26:49 +0000

    Tutorial

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int radius,diameter;
+	float circumference,area;
+	printf("Enter the radius:");
+	scanf("%d\n",&radius);
+	
+	diameter=radius*2;
+	circumference=2*3.14*radius;
+	area=3.14*radius*radius;
+	
+	printf("Diameter is:%d\n",diameter);
+	printf("Circumference is:%.2f\n",circumference);
+	printf("Area is:%.2f\n",area);
+  
   
   return 0;
 }
